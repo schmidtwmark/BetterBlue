dec385a35eceb74ec0633d0d31d2c431fcdae1c9
boe0011m <m.boehm@prosiebensat1.com>
2026-04-27 20:17:02 +0200

    Add handling for non ccs2 and improvements
    (cherry picked from commit 3787d74d6498a6c2be6eba44e6ae75a15b662982)

--- BetterBlue/Utility/VehicleUtility.swift
+++ BetterBlue/Utility/VehicleUtility.swift
@@ -30,6 +30,7 @@ extension BBVehicle {
             generation: generation,
             odometer: odometer,
             vehicleKey: vehicleKey,
+            ccs2Supported: ccs2Supported
         )
     }
 }
